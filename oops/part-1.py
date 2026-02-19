cb45e74cf3df35c1c114c156e9302913d5c2f24c
Yasir Soomro <your-email@example.com>
2026-02-20 03:29:09 +0500

    oops practice tasks

--- oops/part-1.py
+++ oops/part-1.py
@@ -1,2 +1,29 @@
 
 print ("OOPs basic to advanced")
+from array import *
+
+# practice tasks in OOPS 
+
+class Users :
+    def __init__(self , name , email):
+
+        self.name = name 
+        self.email = email
+
+    def display_info (self):
+        print(f"User : {self.name} | {self.email}")
+
+
+users =  [ 
+Users("afzal", "afzal@ccc.com"),
+Users("yasir", "yasir@ccc.com"),
+Users("nasir", "nasir@ccc.com"),
+Users("nasir", "nasir@ccc.com"),
+Users("nasir", "nasir@ccc.com"),
+Users("nasir", "nasir@ccc.com"),
+Users("nasir", "nasir@ccc.com"),
+ ]
+
+for usr in users:
+    usr.display_info()
+         
